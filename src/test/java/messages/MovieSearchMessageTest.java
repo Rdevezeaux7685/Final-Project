a20773c4dc29e148c1855e2b427ebf2caf8a82c9
Rdevezeaux7685 <s566225@nwmissouri.edu>
2023-11-14 18:42:50 -0600

    Adding test for MovieSearchMessageTest.java

--- src/test/java/messages/MovieSearchMessageTest.java
+++ src/test/java/messages/MovieSearchMessageTest.java
@@ -1,4 +1,15 @@
+package messages;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MovieSearchMessageTest {
-  
+
+    @Test
+    void testMovieSearchMessageConstructorAndGetters() {
+        String query = "Action";
+        MovieSearchMessage message = new MovieSearchMessage(query);
+        assertEquals(query, message.getQuery());
+    }
 }
