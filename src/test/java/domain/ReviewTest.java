60391525c903ae3e2ab9de8b74cb3036983cdbba
Rdevezeaux7685 <s566225@nwmissouri.edu>
2023-11-14 18:40:50 -0600

    Adding test for ReviewTest.java

--- src/test/java/domain/ReviewTest.java
+++ src/test/java/domain/ReviewTest.java
@@ -1,4 +1,40 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ReviewTest {
-  
+
+    @Test
+    void testReviewConstructorAndGetters() {
+        Review review = new Review(1, "Great movie", 5);
+
+        assertEquals(1, review.getMovie());
+        assertEquals("Great movie", review.getReview());
+        assertEquals(5, review.getScore());
+    }
+
+    @Test
+    void testEqualsAndHashCode() {
+        Review review1 = new Review(1, "Great movie", 5);
+        Review review2 = new Review(2, "Great movie", 5);
+        Review review3 = new Review(1, "Another movie", 5);
+        Review review4 = new Review(1, "Great movie", 5);
+
+        assertTrue(review1.equals(review4));
+    }
+
+    @Test
+    void testSetId() {
+        Review review = new Review(1, "Great movie", 5);
+        review.setId(42);
+
+    }
+
+    @Test
+    void testToString() {
+        Review review = new Review(1, "Great movie", 5);
+        assertEquals("Review{movie='1', review='Great movie', id=-1}", review.toString());
+    }
 }
