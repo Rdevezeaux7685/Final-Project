62a0d149460dd26a54c13cf8dc3ec2ce8a9c3cdc
Rdevezeaux7685 <s566225@nwmissouri.edu>
2023-11-14 18:39:29 -0600

    Adding test for MovieServiceTest.java

--- src/test/java/domain/MovieServiceTest.java
+++ src/test/java/domain/MovieServiceTest.java
@@ -1,39 +1,28 @@
 package domain;
 
-import data.MockMovieRepo;
-import data.MockReviewRepo;
-import data.MockUserRepo;
+import data.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import util.MovieException;
 
 import java.util.List;
+import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MovieServiceTest {
 
-    MovieService service = new MovieService(
-            new MockUserRepo(), new MockReviewRepo(), new MockMovieRepo()
-    );
-    User user = new User("romane", "test");
+    MovieService service;
 
     @BeforeEach
-    void logUser(){
+    void setUp() {
+        service = new MovieService(
+                new MockUserRepo(), new MockReviewRepo(), new MockMovieRepo()
+        );
+        assertThrows(MovieException.class, () -> service.getReviews());
         service.register("romane", "test");
-        service.login(user.getUsername(), user.getPassword());
-    }
-
-    @Test
-    void register() {
-        assertTrue(service.login("romane", "test"));
-    }
-
-
-    @Test
-    void getMovies() {
-        List<Movie> movies = service.getMovies(user.getUsername());
-        assertTrue(movies.contains(  new Movie(1, "film 1", "")));
+        service.login("romane", "test");
+        assertFalse(service.login("romane", "nopeNotThis"));
     }
 
     @Test
@@ -41,6 +30,35 @@ class MovieServiceTest {
         service.addReview(new Movie(1, "some movie", ""), "Amazing", 7);
         service.addReview(new Movie(3, "Heyyy", ""), "qewqewe", 4);
         assertEquals(2, service.getReviews().size());
+    }
 
+    @Test
+    void login() {
+        assertTrue(service.login("romane", "test"));
+        assertFalse(service.login("romane", "wrongPassword"));
+    }
+
+    @Test
+    void testRegister() {
+        assertTrue(service.login("romane", "test"));
+    }
+
+    @Test
+    void getReviews() {
+        Set<Review> reviewList = service.getReviews();
+        assertTrue(reviewList.isEmpty());
+    }
+
+    @Test
+    void testGetMovies() {
+        List<Movie> movies = service.getMovies("romane");
+        assertTrue(movies.contains(new Movie(1, "film 1", "")));
+    }
+
+    @Test
+    void testAddReview() {
+        service.addReview(new Movie(1, "some movie", ""), "Amazing", 7);
+        service.addReview(new Movie(3, "Heyyy", ""), "qewqewe", 4);
+        assertEquals(2, service.getReviews().size());
     }
 }
