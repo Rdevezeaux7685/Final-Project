333121683b84fac0685f8327007216cba57b6e8f
Rdevezeaux7685 <s566225@nwmissouri.edu>
2023-11-15 13:35:49 -0600

    Adding a generated Annotation to remove the Program class from the Jacoco coverage, this class was only use to encrypt the username and password before putting them in config.properties, should have been removed

--- src/main/java/util/cli/Program.java
+++ src/main/java/util/cli/Program.java
@@ -1,10 +1,11 @@
 package util.cli;
 import util.Crypto;
+import util.Generated;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
+@Generated
 public class Program {
     private static final Logger LOGGER = Logger.getLogger(Program.class.getName());
 
