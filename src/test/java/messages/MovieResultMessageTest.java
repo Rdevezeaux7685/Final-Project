94ef2a9d6df033eddf22af8caab64fb2caac3593
Rdevezeaux7685 <s566225@nwmissouri.edu>
2023-11-14 18:42:08 -0600

    Adding test for MovieResultMessageTest.java

--- src/test/java/messages/MovieResultMessageTest.java
+++ src/test/java/messages/MovieResultMessageTest.java
@@ -1,4 +1,37 @@
+package messages;
+
+import domain.Movie;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MovieResultMessageTest {
-  
+
+    @Test
+    void testMovieResultMessageConstructorAndGetters() {
+        List<Movie> movies = List.of(
+                new Movie(1, "Movie 1", "cover1.jpg"),
+                new Movie(2, "Movie 2", "cover2.jpg")
+        );
+
+        MovieResultMessage message = new MovieResultMessage(new ArrayList<>(movies));
+
+        assertEquals(movies, message.getResults());
+    }
+
+    @Test
+    void testToString() {
+        List<Movie> movies = List.of(
+                new Movie(1, "Movie 1", "cover1.jpg"),
+                new Movie(2, "Movie 2", "cover2.jpg")
+        );
+
+        MovieResultMessage message = new MovieResultMessage(new ArrayList<>(movies));
+
+        assertEquals("MovieResultMessage{results=" + movies + '}', message.toString());
+
+    }
 }
