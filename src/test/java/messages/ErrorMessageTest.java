2ddec6d31a908956714fe862ef6051226e4e8a17
Rdevezeaux7685 <s566225@nwmissouri.edu>
2023-11-14 18:41:52 -0600

    Adding test for ErrorMessage.java

--- src/test/java/messages/ErrorMessageTest.java
+++ src/test/java/messages/ErrorMessageTest.java
@@ -1,4 +1,17 @@
+package messages;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ErrorMessageTest {
-  
+
+    @Test
+    void testErrorMessageConstructorAndGetters() {
+        String errorMessage = "Oops, Something went wrong";
+        ErrorMessage message = new ErrorMessage(errorMessage);
+        assertEquals(errorMessage, message.getMessage());
+    }
+
+
 }
