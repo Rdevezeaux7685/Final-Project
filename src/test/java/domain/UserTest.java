ae93e51f766566ffc80c385586f302395c1d3f38
Rdevezeaux7685 <s566225@nwmissouri.edu>
2023-11-14 18:41:13 -0600

    Adding test for USerTest.java

--- src/test/java/domain/UserTest.java
+++ src/test/java/domain/UserTest.java
@@ -1,4 +1,29 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserTest {
-  
+
+    @Test
+    void testUserConstructorAndGetters() {
+        User user = new User("testUser", "password");
+
+        assertEquals("testUser", user.getUsername());
+        assertEquals("password", user.getPassword());
+    }
+
+    @Test
+    void testEqualsAndHashCode() {
+        User user1 = new User("user1", "password");
+        User user2 = new User("user1", "password");
+        User user3 = new User("user2", "password");
+
+        assertTrue(user1.equals(user2));
+        assertFalse(user1.equals(user3));
+        assertEquals(user1.hashCode(), user2.hashCode());
+        assertNotEquals(user1.hashCode(), user3.hashCode());
+    }
+
 }
